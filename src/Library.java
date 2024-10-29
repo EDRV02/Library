788cd2748e9da4a7ad1e792c28166787dc0b2072
EDRV02 <eduardo.diazriovarez@hotmail.com>
2024-10-29 13:50:10 +0000

    addBook method added

--- src/Library.java
+++ src/Library.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 
 public class Library {
-    private static Map<String, Books> books = new HashMap<>();
+    private static Map<String, Integer> books = new HashMap<String, Integer>();
     private static Map<String, Patrons> patrons = new HashMap<>();
     private static int bookIdCounter = 1;
     private static int patronIdCounter = 1;
@@ -14,7 +14,8 @@ public class Library {
         String command;
 
         while (true) {
-            System.out.println("Enter command:\n");
+            System.out.println("Enter command:\n" +
+                    "addBook\n");
 
             command = scanner.nextLine();
 
@@ -28,7 +29,9 @@ public class Library {
     }
     private static void handleCommand(String command, Scanner scanner) {
         switch (command) {
-            //add book
+            case "addBook":
+                addBook(scanner);
+                break;
             //add patron
             //search book
             //borrow book
@@ -39,5 +42,19 @@ public class Library {
         }
     }
 
+    private static void addBook(Scanner scanner) {
+        System.out.println("Enter name, ISB: ");
+        String[] bookInfo = scanner.nextLine().split(",");
+        String bookName = bookInfo[0].trim();
+        int isb = Integer.parseInt(bookInfo[1].trim());
+        int bookId = bookIdCounter++;
+        Books book = new Books(bookName, isb);
+        books.put(bookName, isb);
+        System.out.println("Book added: " + bookName + " ISB: " + isb);
+
+
+
+
     }
-}
+
+    }
